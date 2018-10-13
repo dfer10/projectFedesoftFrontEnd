58c4d13297e5da89497e252289900d37c786b3d5
dfer10 <didarragah@ucentral.edu.co>
2018-10-12 21:33:07 -0500

    component login user

---
+++ src/app/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styles: ['.sr {color: red;}']
+})
+export class LoginComponent {
+    email = 'login1';
+}
